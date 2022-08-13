1ad865dc4b42b424fc2a4fd761ed2cafdfcb37a5
Lee Jaeha <108874515+jaehafe@users.noreply.github.com>
2022-08-13 22:45:40 +0900

    questions

---
+++ 07-questions/app.js
@@ -0,0 +1,18 @@
+const questions = document.querySelectorAll(".question");
+
+questions.forEach((question) => {
+  const btn = question.querySelector(".question-btn");
+  // console.log(btn);
+
+  btn.addEventListener("click", () => {
+    // console.log(question);
+
+    questions.forEach((item) => {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+
+    question.classList.toggle("show-text");
+  });
+});
