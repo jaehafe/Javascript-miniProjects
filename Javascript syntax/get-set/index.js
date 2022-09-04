0f71e9c8509189f9f777b7edae6a73aac6b167eb
Lee Jaeha <108874515+jaehafe@users.noreply.github.com>
2022-09-04 19:35:51 +0900

    get and set

---
+++ Javascript syntax/get-set/index.js
@@ -0,0 +1,18 @@
+// getter setter
+
+const person = {
+  firstName: 'Adam',
+  lastName: 'Lee',
+  get fullName() {
+    return `${person.firstName} ${person.lastName}`;
+  },
+  set fullName(value) {
+    const parts = value.split(' ');
+    this.firstName = parts[0];
+    this.lastName = parts[1];
+  },
+};
+
+person.fullName = 'John Smith';
+
+console.log(person);
