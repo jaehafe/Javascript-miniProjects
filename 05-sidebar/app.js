61b8dfd8196a4e392148e755c6aad65380177cbc
Lee Jaeha <108874515+jaehafe@users.noreply.github.com>
2022-09-07 00:45:43 +0900

    sidebar // 수정 및 주석 추가

--- 05-sidebar/app.js
+++ 05-sidebar/app.js
@@ -1,16 +1,11 @@
-const toggleBtn = document.querySelector(".sidebar-toggle");
-const closeBtn = document.querySelector(".close-btn");
-const sidebar = document.querySelector(".sidebar");
+const sidebarToggle = document.querySelector('.sidebar-toggle');
+const sidebar = document.querySelector('.sidebar');
+const closeBtn = document.querySelector('.close-btn');
 
-toggleBtn.addEventListener("click", () => {
-  // if (sidebar.classList.contains("show-sidebar")) {
-  //   sidebar.classList.remove("show-sidebar");
-  // } else {
-  //   sidebar.classList.add("show-sidebar");
-  // }
-  sidebar.classList.toggle("show-sidebar");
+sidebarToggle.addEventListener('click', () => {
+  sidebar.classList.toggle('show-sidebar');
 });
 
-closeBtn.addEventListener("click", () => {
-  sidebar.classList.remove("show-sidebar");
+closeBtn.addEventListener('click', () => {
+  sidebar.classList.remove('show-sidebar');
 });
