eebcb8804c38acc41ae96f807d05a51c13106a0a
Lee Jaeha <108874515+jaehafe@users.noreply.github.com>
2022-09-07 00:46:45 +0900

    modal
     // + Hide modal on outside click

---
+++ 06-modal/app.js
@@ -0,0 +1,22 @@
+// select modal-btn,modal-overlay,close-btn
+// listen for click events on modal-btn and close-btn
+// when user clicks modal-btn add .open-modal to modal-overlay
+// when user clicks close-btn remove .open-modal from modal-overlay
+
+const modalBtn = document.querySelector('.modal-btn');
+const closeBtn = document.querySelector('.close-btn');
+const modal = document.querySelector('.modal-overlay');
+const modalContainer = document.querySelector('.modal-container');
+
+modalBtn.addEventListener('click', () => {
+  modal.classList.add('open-modal');
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.classList.remove('open-modal');
+});
+
+// Hide modal on outside click
+window.addEventListener('click', (e) => {
+  e.target === modal ? modal.classList.remove('open-modal') : false;
+});
