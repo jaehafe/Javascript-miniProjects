ab6243b596b3c9bc355a1f88683b60ceb826a1dd
Lee Jaeha <108874515+jaehafe@users.noreply.github.com>
2022-09-07 15:20:11 +0900

    questions and menu

--- 07-questions/app.js
+++ 07-questions/app.js
@@ -1,18 +1,26 @@
-const questions = document.querySelectorAll(".question");
+//using selectors inside the element
+const questions = document.querySelectorAll('.question');
 
 questions.forEach((question) => {
-  const btn = question.querySelector(".question-btn");
-  // console.log(btn);
-
-  btn.addEventListener("click", () => {
-    // console.log(question);
-
+  // console.log(question);
+  const btn = question.querySelector('.question-btn');
+  btn.addEventListener('click', () => {
     questions.forEach((item) => {
       if (item !== question) {
-        item.classList.remove("show-text");
+        item.classList.remove('show-text');
       }
     });
 
-    question.classList.toggle("show-text");
+    question.classList.toggle('show-text');
   });
 });
+
+// traversing the dom
+// const btns = document.querySelectorAll('.question-btn');
+
+// btns.forEach((btn) => {
+//   btn.addEventListener('click', (e) => {
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle('show-text');
+//   });
+// });
