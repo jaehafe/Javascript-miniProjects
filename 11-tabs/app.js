65877a5eeccb53ae9f6b688daae19870f4b22500
Lee Jaeha <108874515+jaehafe@users.noreply.github.com>
2022-09-08 12:58:23 +0900

    taps, slider

--- 11-tabs/app.js
+++ 11-tabs/app.js
@@ -1,22 +1,22 @@
-const about = document.querySelector(".about");
-const btns = document.querySelectorAll(".tab-btn");
-const articles = document.querySelectorAll(".content");
+const about = document.querySelector('.about');
+const btns = document.querySelectorAll('.tab-btn');
+const articles = document.querySelectorAll('.content');
 
-about.addEventListener("click", function (e) {
-  console.log(e.target);
+about.addEventListener('click', function (e) {
   const id = e.target.dataset.id;
+
   if (id) {
-    // remove selected from other buttons
-    btns.forEach(function (btn) {
-      btn.classList.remove("active");
+    btns.forEach((btn) => {
+      btn.classList.remove('active');
     });
-    e.target.classList.add("active");
+    e.target.classList.add('active');
 
     // hide other articles
-    articles.forEach(function (article) {
-      article.classList.remove("active");
+    articles.forEach((article) => {
+      article.classList.remove('active');
     });
+
     const element = document.getElementById(id);
-    element.classList.add("active");
+    element.classList.add('active');
   }
 });
