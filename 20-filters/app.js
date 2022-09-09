47d610e6d43b567e49520883266aaf8a3fe3682e
Lee Jaeha <108874515+jaehafe@users.noreply.github.com>
2022-09-09 21:33:33 +0900

    filters

--- 20-filters/app.js
+++ 20-filters/app.js
@@ -1,40 +1,41 @@
 let filteredProducts = [...products];
 
-const productsContainer = document.querySelector(".products-container");
+const productsContainer = document.querySelector('.products-container');
 
 const displayProducts = () => {
+  // if statement
   if (filteredProducts.length < 1) {
-    productsContainer.innerHTML = `<h6>Sorry, no products matched your search</h6>`;
+    productsContainer.inner = `
+      <h6>Sorry, no products matched your search</h6>
+    `;
     return;
   }
-
   productsContainer.innerHTML = filteredProducts
-    .map((product) => {
-      const { id, title, image, price } = product;
+    .map((products) => {
+      const { id, title, image, price } = products;
       return `
-        <article class="product" data-id="${id}">
-          <img
-            src="${image}"
-            class="product-img img"
-            alt="${title}"
-          />
-          <footer>
-            <h5 class="product-name">${title}</h5>
-            <span class="product-price">${price}</span>
-          </footer>
-        </article>`;
+      <article class="product">
+        <img
+          src=${image}
+          class="product-img img"
+        />
+        <footer>
+          <h5 class="product-name">${title}</h5>
+          <span class="product-price">$${price}</span>
+        </footer>
+      </article>
+      `;
     })
-    .join("");
+    .join('');
 };
 
 displayProducts();
 
-// Text Filter
+// filter
+const form = document.querySelector('.input-form');
+const searchInput = document.querySelector('.search-input');
 
-const form = document.querySelector(".input-form");
-const searchInput = document.querySelector(".search-input");
-
-form.addEventListener("keyup", () => {
+form.addEventListener('keyup', () => {
   const inputValue = searchInput.value;
   filteredProducts = products.filter((product) => {
     return product.title.toLowerCase().includes(inputValue);
@@ -42,42 +43,39 @@ form.addEventListener("keyup", () => {
   displayProducts();
 });
 
-// console.log(
-//   products.filter((product) => {
-//     return product.title.toLowerCase().includes('');
-//   })
-// );
+// filter buttons
+// Set은 중복이 허용되지 않는 객체
 
-// Filter Buttons
-
-const companiesDOM = document.querySelector(".companies");
+const companiesDOM = document.querySelector('.companies');
 
 const displayButtons = () => {
   const buttons = [
-    "all",
+    'all',
     ...new Set(products.map((product) => product.company)),
   ];
-  // console.log("buttons", buttons);
   companiesDOM.innerHTML = buttons
     .map((company) => {
-      return `<button class='company-btn' data-id="${company}">${company}</button>`;
+      return `
+    <button class="company-btn" data-id="${company}">${company}</button>
+    `;
     })
-    .join("");
+    .join('');
 };
 
 displayButtons();
 
-companiesDOM.addEventListener("click", (e) => {
+// click and filter by company
+companiesDOM.addEventListener('click', (e) => {
   const el = e.target;
-  if (el.classList.contains("company-btn")) {
-    if (el.dataset.id === "all") {
+  if (el.classList.contains('company-btn')) {
+    if (el.dataset.id === 'all') {
       filteredProducts = [...products];
     } else {
       filteredProducts = products.filter((product) => {
         return product.company === el.dataset.id;
       });
     }
-    searchInput.value = "";
+    searchInput.value = '';
     displayProducts();
   }
 });
