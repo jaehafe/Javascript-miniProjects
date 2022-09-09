93266883c9ff81f753321e06f4815fbbe5a22c24
Lee Jaeha <108874515+jaehafe@users.noreply.github.com>
2022-09-09 16:06:18 +0900

    dark mode

--- 19-darkmode/app.js
+++ 19-darkmode/app.js
@@ -1,25 +1,27 @@
-const toggleBtn = document.querySelector(".btn");
-const articlesContainer = document.querySelector(".articles");
+const toggleBtn = document.querySelector('.btn');
+const articlesContainer = document.querySelector('.articles');
 
-toggleBtn.addEventListener("click", () => {
-  document.documentElement.classList.toggle("dark-theme");
+toggleBtn.addEventListener('click', () => {
+  document.documentElement.classList.toggle('dark-theme'); // html 전체에 dark-theme class 추가
 });
 
 const articlesData = articles
   .map((article) => {
     const { title, date, length, snippet } = article;
-    const formatDate = moment(date).format("MMMM Do, YYYY");
-    return `<article class="post">
-          <h2>${title}</h2>
-          <div class="post-info">
-            <span>${formatDate}</span>
-            <span>${length} min read</span>
-          </div>
-          <p>
-            ${snippet}
-          </p>
-        </article>`;
+    // format date, moment.js
+    const formatDate = moment(date).format('MMMM Do, YYYY');
+    return `
+      <article class="post">
+        <h2>${title}</h2>
+        <div class="post-info">
+          <span>${formatDate}</span>
+          <span>${length} min read</span>
+        </div>
+        <p>
+          ${snippet}
+        </p>
+      </article>`;
   })
-  .join("");
+  .join('');
 
 articlesContainer.innerHTML = articlesData;
