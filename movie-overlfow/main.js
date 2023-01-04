cd8cd14e0c47295875273a257deda21c5ccdc958
Jaeha Lee <wogkdkrm112@gmail.com>
2023-01-05 01:40:18 +0900

    fix: Fix breakpoint mixin error
    1. Fix breakpoint error
    2. Add movie card section html, scss

--- movie-overlfow/main.js
+++ movie-overlfow/main.js
@@ -1,1 +1,12 @@
 import './scss/style.scss';
+
+// close when click outside
+// const specifiedElement = document.getElementById('a')
+
+// document.addEventListener('click', event => {
+//   const isClickInside = specifiedElement.contains(event.target)
+
+//   if (!isClickInside) {
+//     // The click was OUTSIDE the specifiedElement, do something
+//   }
+// })
