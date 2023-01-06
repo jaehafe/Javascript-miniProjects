55df984be1b5f93a52ce75056dff2736744e4c64
Jaeha Lee <wogkdkrm112@gmail.com>
2023-01-06 20:48:06 +0900

    fix: Fixing bug not showing movieTemplate

--- movie-overlfow/main.js
+++ movie-overlfow/main.js
@@ -15,6 +15,7 @@ import './scss/style.scss';
 const openModalButtons = document.querySelectorAll('[data-modal-target]');
 const closeModalButtons = document.querySelectorAll('[data-close-button]');
 const overlay = document.getElementById('overlay');
+const $ = (selector) => document.querySelector(selector);
 
 openModalButtons.forEach((button) => {
   button.addEventListener('click', () => {
@@ -49,4 +50,98 @@ const closeModal = (modal) => {
   overlay.classList.remove('active');
 };
 
+const errorHandle = {
+  loadingDOM() {
+    return ($(
+      '.section__container-movie-card-container'
+    ).innerHTML = `<div class="loading"></div>`);
+  },
+
+  errorDOM() {
+    return ($('.section__container-movie-card-container').innerHTML =
+      '<p class="error">there was an error</p>');
+  },
+};
+
+const fetchQuestions = async () => {
+  $('.questionsDOM').innerHTML = `<div class='loading'></div>`;
+  try {
+    const res = await getMovies();
+    const { items } = await res.json();
+    console.log(items);
+    return items;
+  } catch (err) {
+    console.log(err);
+    $('.questionsDOM').innerHTML = '<p class="error">there was an error</p>';
+  }
+};
+
 // api
+const getMovies = async (title, year = '', page = 1) => {
+  const s = `&s=${title}`;
+  const y = `&y=${year}`;
+  const p = `&page=${page}`;
+  const res = await fetch(
+    `https://omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}${s}${y}${p}`
+  );
+  const data = await res.json();
+
+  if (data.Response === 'True') {
+    const { Search: movies, totalResults } = data;
+    console.log(movies);
+
+    const movieTemplate = movies
+      .map((movie) => {
+        const { Poster, Title, Year, imdbID } = movie;
+        return `
+                <div class="section__container-movie-card" data-movie-id="${imdbID}">
+                  <button>
+                    <img
+                      data-modal-target="#modal"
+                      src="${Poster}"
+                      alt=""
+                      class="section__container-movie-card--img"
+                    />
+                  </button>
+                  <div class="flex">
+                    <div class="section__container-movie-card--thumb">
+                      <p class="section__container-movie-card--year">${Year}</p>
+                      <h2 class="section__container-movie-card--title">${Title}</h2>
+                    </div>
+                    <div class="section__container-movie-card--btn">
+                      <button
+                        class="section__container-movie-card--btn-add movie-add-button"
+                      >
+                        추가
+                      </button>
+                      <button
+                        class="section__container-movie-card--btn-remove movie-remove-button"
+                      >
+                        삭제
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                `;
+      })
+      .join('');
+    $('.section__container-movie-card-container').insertAdjacent(
+      'afterbegin',
+      movieTemplate
+    );
+  }
+  return data.Error;
+};
+
+getMovies('spider');
+
+// const displayMovies = async (title) => {
+//   const movieList = await getMovies(title);
+//   console.log(movieList.movies);
+//   console.log(movieList.totalResults);
+// };
+// displayMovies('title');
+
+// movies.map((movie) => {
+//   console.log(movie);
+// });
