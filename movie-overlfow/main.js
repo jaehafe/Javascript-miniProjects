83560fa06e740a760ef1f52a197203d420a5217a
Jaeha Lee <wogkdkrm112@gmail.com>
2023-01-10 22:05:32 +0900

    feat: 상세페이지 구현 전까지 진행

--- movie-overlfow/main.js
+++ movie-overlfow/main.js
@@ -1,28 +1,16 @@
 import './scss/style.scss';
 
-// close when click outside
-// const specifiedElement = document.getElementById('a')
-
-// document.addEventListener('click', event => {
-//   const isClickInside = specifiedElement.contains(event.target)
-
-//   if (!isClickInside) {
-//     // The click was OUTSIDE the specifiedElement, do something
-//   }
-// })
-
-// modal close
 const openModalButtons = document.querySelectorAll('[data-modal-target]');
 const closeModalButtons = document.querySelectorAll('[data-close-button]');
 const overlay = document.getElementById('overlay');
-const cardContainer = document.querySelector(
-  '.section__container-movie-card-container'
-);
-const $ = (selector) => document.querySelector(selector);
 
 openModalButtons.forEach((button) => {
+  console.log(button);
   button.addEventListener('click', () => {
+    // data-modal-target="#modal"
     const modal = document.querySelector(button.dataset.modalTarget);
+    console.log(modal);
+
     openModal(modal);
   });
 });
@@ -52,70 +40,102 @@ const closeModal = (modal) => {
   modal.classList.remove('active');
   overlay.classList.remove('active');
 };
-
-const errorHandle = {
-  loadingDOM() {
-    return ($(
-      '.section__container-movie-card-container'
-    ).innerHTML = `<div class="loading"></div>`);
-  },
-
-  errorDOM() {
-    return ($('.section__container-movie-card-container').innerHTML =
-      '<p class="error">there was an error</p>');
-  },
-};
-
 //
 
-const fetchQuestions = async () => {
-  $('.questionsDOM').innerHTML = `<div class='loading'></div>`;
-  try {
-    const res = await getMovies();
-    const { items } = await res.json();
-    console.log(items);
-    return items;
-  } catch (err) {
-    console.log(err);
-    $('.questionsDOM').innerHTML = '<p class="error">there was an error</p>';
+const $ = (selector) => document.querySelector(selector);
+
+const $searchBtn = $('#searchBtn');
+const $searchInput = $('#searchInput');
+const $movies = $('.section__container-movie-card-container');
+const $selectedYear = $('.main__search--options-select-year');
+const $selectedPage = $('.main__search--options-page');
+let $searchTotalResult = $('.main__search--result');
+const $movieDetail = $('.movie-detail');
+const $scrollTop = $('.scroll-top');
+
+let title;
+let year;
+let page = 1;
+
+async function getMovies(title, year, page) {
+  const s = `&s=${title}`;
+  const y = `&y=${year}`;
+  const p = `&page=${page}`;
+  const res = await fetch(`https://omdbapi.com/?apikey=7035c60c${s}${y}${p}`);
+  const { Search: movies, totalResults } = await res.json();
+  return { movies, totalResults };
+}
+
+// 영화 상세페이지 가져오기
+const getMovieDetail = async (movieId) => {
+  const res = await fetch(
+    `https://omdbapi.com/?apikey=7035c60c&i=${movieId}&plot=full`
+  );
+  const movieDetail = await res.json();
+  return movieDetail;
+};
+
+// $movieDetail.addEventListener('click', () => {
+//   openMovieDetail();
+// });
+
+const openMovieDetail = async () => {};
+
+const findMovies = async () => {
+  // reset movie list
+  $movies.innerHTML = '';
+  title = $searchInput.value;
+  if (title === '' || title.length < 3) {
+    alert('3글자 이상 입력해주세요.');
+  }
+
+  // 검색 option(page, year) 값 가져오기
+  page = Number(
+    $selectedPage.options[$selectedPage.selectedIndex].dataset.selectValue
+  );
+  year = $selectedYear.options[$selectedYear.selectedIndex].value;
+  // console.log('page', page);
+  // console.log('year', year);
+
+  // 검색 option에 따른 영화 가져오기
+  for (let i = 1; i <= page; i++) {
+    const { movies, totalResults } = await getMovies(title, year, i);
+    console.log(movies);
+
+    // handleErrNLoading.loadingDOM();
+    try {
+      movies
+        ? displayMovies(movies, totalResults)
+        : handleErrNLoading.undefinedDOM();
+    } catch (err) {
+      handleErrNLoading.errorDOM();
+      console.log(err);
+    }
   }
 };
 
-// api
-const getMovies = async (title = '', year = '', page = 2) => {
-  $(
-    '.section__container-movie-card-container'
-  ).innerHTML = `<div class='loading'></div>`;
-  try {
-    const s = `&s=${title}`;
-    const y = `&y=${year}`;
-    const p = `&page=${page}`;
-    const res = await fetch(
-      `https://omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}${s}${y}${p}`
-    );
-    const data = await res.json();
-
-    if (data.Response === 'True') {
-      const { Search: movies, totalResults } = data;
-      console.log(movies);
-
-      const movieTemplate = movies
-        .map((movie) => {
-          const { Poster, Title, Year, imdbID } = movie;
-          return `
+const displayMovies = async (movies) => {
+  const movieList = await movies
+    .map((movie) => {
+      const { Poster, Title, Year, imdbID } = movie;
+      return `
                 <div class="section__container-movie-card" data-movie-id="${imdbID}">
-                  <button>
+                  <button type="button" class="movie-detail" data-modal-target="#modal">
                     <img
-                      data-modal-target="#modal"
-                      src="${Poster}"
-                      alt=""
+            
+                      src="${Poster === 'N/A' ? 'image_not_found.png' : Poster}"
+                      alt="${Title}"
                       class="section__container-movie-card--img"
                     />
                   </button>
                   <div class="flex">
                     <div class="section__container-movie-card--thumb">
                       <p class="section__container-movie-card--year">${Year}</p>
-                      <h2 class="section__container-movie-card--title">${Title}</h2>
+                      <h2 class="section__container-movie-card--title">${
+                        Title.length > 10
+                          ? Title.substring(0, 10).concat(' ...')
+                          : Title
+                      }</h2>
                     </div>
                     <div class="section__container-movie-card--btn">
                       <button
@@ -132,39 +152,109 @@ const getMovies = async (title = '', year = '', page = 2) => {
                   </div>
                 </div>
                 `;
-        })
-        .join('');
-      cardContainer.innerHTML = movieTemplate;
+    })
+    .join('');
 
-      let search = $('#searchInput').value;
-      $(
-        '.main__search--result'
-      ).innerHTML = `${search}이(가) 총 ${totalResults} 개 검색되었습니다.`;
-    }
+  $movies.innerHTML += movieList;
+
+  updateTotalResults();
+};
+
+// 검색어, 검색영화 총 개수 업데이트 함수
+const updateTotalResults = async () => {
+  handleErrNLoading.loadingDOM();
+  try {
+    const { totalResults } = await getMovies(title, year);
+    // const { totalResults } = await getMovies();
+    let movieTitle = $searchInput.value;
+    // console.log($movies.children.length);
+    return ($searchTotalResult.innerHTML = `${movieTitle}이(가) ${totalResults} 개 중 ${$movies.children.length}개 검색되었습니다.`);
   } catch (err) {
+    handleErrNLoading.errorDOM();
     console.log(err);
-    $('.section__container-movie-card-container').innerHTML =
-      '<p class="error">there was an error</p>';
-    throw err;
   }
 };
 
-$('.main__search-container').addEventListener('submit', (e) => {
+// 검색 click, Enter시 findMovies
+$searchBtn.addEventListener('click', async (e) => {
   e.preventDefault();
-  let search = $('#searchInput').value;
-  if (search) {
-    getMovies(search);
-    search = '';
-  }
+  findMovies();
 });
 
-// const displayMovies = async (title) => {
-//   const movieList = await getMovies(title);
-//   console.log(movieList.movies);
-//   console.log(movieList.totalResults);
-// };
-// displayMovies('title');
+$searchBtn.addEventListener('keypress', (e) => {
+  if (e.key !== 'Enter') {
+    return;
+  }
+  findMovies();
+});
 
-// movies.map((movie) => {
-//   console.log(movie);
-// });
+// 영화 년도 생성
+for (let i = 2023; i >= 1980; i--) {
+  const yearOpt = document.createElement('option');
+  yearOpt.value = i;
+  yearOpt.textContent = i;
+  $selectedYear.append(yearOpt);
+}
+
+// 무한 스크롤
+const io = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    // 검색했을 때만 observe 관찰
+    if (entry.isIntersecting && $movies.children.length > 1) {
+      getMoreMovies();
+    }
+  });
+});
+io.observe($('#infinite-scroll__target'));
+if ($movies === undefined) {
+  io.unobserve($('#infinite-scroll__target'));
+  console.log(io.unobserve($('#infinite-scroll__target')));
+}
+
+// 더 많은 영화 가져오기
+const getMoreMovies = async () => {
+  page += 1;
+  const { movies } = await getMovies(title, year, page);
+
+  try {
+    movies && displayMovies(movies);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const handleErrNLoading = {
+  loadingDOM: () => {
+    return ($('.main__search--result').innerHTML = ` <svg
+    class="spinner active"
+    width="30px"
+    height="30px"
+    viewBox="0 0 66 66"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle
+      class="path"
+      fill="none"
+      stroke-width="6"
+      stroke-linecap="round"
+      cx="33"
+      cy="33"
+      r="30"
+    ></circle>
+  </svg>`);
+  },
+
+  errorDOM: () => {
+    return ($(
+      '.main__search--result'
+    ).innerHTML = `<p class="error">에러가 발생했습니다.</p>`);
+  },
+
+  undefinedDOM: () => {
+    return ($(
+      '.main__search--result'
+    ).innerHTML = `<p class="error">관련 영화가 없습니다.</p>`);
+  },
+};
+
+// modal
